37d5fcbb9b61829830e166f122f42410e90088a7
Matthew Gaalswyk <github@matthew.gaalswyk.com>
2015-04-14 10:46:23 -0500

    - change to random normal in create_image - add c++0x and remove -O2 for ci

--- create_image.cpp
+++ create_image.cpp
@@ -1,5 +1,7 @@
 #include <boost/random/uniform_int.hpp>
 #include <boost/random/mersenne_twister.hpp>
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/variate_generator.hpp>
 
 // image processing & display
 #include "CImg.h"
@@ -9,6 +11,10 @@ using namespace cimg_library;
 struct Point {
   int x,y;
 };
+
+int clamp(int x, int min, int max) {
+  return (x<min) ? min : (x>max) ? max : x;
+}
 
 int main(int argc, char *argv[]) {
   cimg_usage("command line arguments");
@@ -20,21 +26,24 @@ int main(int argc, char *argv[]) {
   CImg<unsigned char> image(width,height,1,1);
 
   // init rng
-  boost::mt19937 rng;
-  boost::uniform_int<> rw(0,width-1);
-  boost::uniform_int<> rh(0,height-1);
-  boost::uniform_int<> noise(0,10);
+  boost::mt19937 rng(time(0));
+  boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
+    rw(rng, boost::uniform_int<>(0,width-1));
+  boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
+    rh(rng, boost::uniform_int<>(0,height-1));
+  boost::variate_generator<boost::mt19937&, boost::normal_distribution<> >
+    noise(rng, boost::normal_distribution<>(0.0,1.0));
 
   cimg_forXY(image,x,y) {
-    image(x,y) = noise(rng) + 128;
+    image(x,y) = clamp(int(noise()*10.24 + 128),0,255);
   }
 
   for(float i=0; i<discs-1; i++) {
-    Point p = Point{rw(rng), rh(rng)};
+    Point p = Point{rw(), rh()};
     cimg_forXY(image,x,y) {
       float radius = (x-p.x)*(x-p.x)+(y-p.y)*(y-p.y);
       if (radius < 100)
-        image(x,y) = noise(rng) + 64;
+        image(x,y) = clamp(int(noise()*10.24 + 64),0,255);
     }
   }
 
--- makefile
+++ makefile
@@ -5,7 +5,7 @@ mcmc: mcmc.cpp
 	g++ -O2 -std=c++0x -lm -lX11 -lpthread -o mcmc mcmc.cpp
 
 ci: create_image.cpp
-	g++ -O2 -std=c++0x -lm -lX11 -lpthread -o ci create_image.cpp
+	g++ -std=c++0x -lm -lX11 -lpthread -o ci create_image.cpp
 
 all: mcmc 
 
