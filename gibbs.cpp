8584ae6bafae40bcae27493f74bc90e11f64eaeb
Matthew Gaalswyk <github@matthew.gaalswyk.com>
2015-04-26 20:56:57 -0500

    - stuff

--- gibbs.cpp
+++ gibbs.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+
+// C headers (ew)
 #include <mpi.h>
+#include <sys/time.h>
 
 // rng & normal/poisson distribution
 #include <boost/random/uniform_int.hpp>
@@ -36,6 +39,7 @@ CImgDisplay main_disp;
 double white[1] = {1.0};
 int world_size, world_rank;
 char processor_name[MPI_MAX_PROCESSOR_NAME];
+bool show_disp = true;
 
 struct Point {
   int x,y;
@@ -104,24 +108,18 @@ double random_normal() {
 
 vector<Point> gibbs_sampling(const Img &image,
   const Img &target, int num_objs, vector<Point> Oxy,
-  const int max_iterations) {
+  const int max_iterations, int diffusion) {
   int T = max_iterations;
   //if (world_rank == 0) printf("\n\t\tGibbs:[Step-100]");
 
-  vector<vector<Point>> AOxy (T);
-  AOxy[0] = Oxy;
-  vector<Point> Cur_Oxy = AOxy[0];
+  vector<Point> Cur_Oxy = Oxy;
   double L1 = likelihood(image,target,Cur_Oxy,num_objs);
 
   for (int t=0; t<T; t++) {
-    /*if (world_rank == 0)
-      printf("\b\b\b\b%03u]", t);
-    cout.flush();*/
     for (int i=0; i<num_objs; i++) {
-      Point Oxy = Cur_Oxy[i];
-      Point Dxy = Oxy;
-      Dxy.x += round(random_normal()*20);
-      Dxy.y += round(random_normal()*20);
+      Point Dxy = Cur_Oxy[i];
+      Dxy.x += round(random_normal()*diffusion);
+      Dxy.y += round(random_normal()*diffusion);
       Dxy = clamp(Dxy,height,width);
       vector<Point> New_Cur_Oxy = Cur_Oxy;
       New_Cur_Oxy[i] = Dxy;
@@ -129,11 +127,9 @@ vector<Point> gibbs_sampling(const Img &image,
       double v = min(1.0,L2/L1);
       double u = random_uniform();
       if (v > u) {
-        Oxy = Dxy;
         Cur_Oxy = New_Cur_Oxy;
         L1 = L2;
       }
-      AOxy[t] = Cur_Oxy;
     }
   }
   //if (world_rank==0) printf("\nDone Gibbs\n");
@@ -152,7 +148,29 @@ void init_mpi() {
     MPI_Get_processor_name(processor_name, &name_len);
 }
 
+// source:
+// http://www.guyrutenberg.com/2007/09/22/profiling-code-using-clock_gettime/
+timespec diff(timespec ts_start, timespec ts_end) {
+  timespec d;
+  if ((ts_end.tv_nsec-ts_start.tv_nsec) < 0) {
+    // if we didn't check this, we'd sometimes get negative nanoseconds
+    // so we borrow a 1 from the seconds and add a billion to the ns
+    d.tv_sec = ts_end.tv_sec - ts_start.tv_sec - 1;
+    d.tv_nsec= 1000000000 + ts_end.tv_nsec - ts_start.tv_nsec;
+  } else {
+    d.tv_sec = ts_end.tv_sec - ts_start.tv_sec;
+    d.tv_nsec= ts_end.tv_nsec - ts_start.tv_nsec;
+  }
+  return d;
+}
+
+
 int main(int argc, char *argv[]) {
+  // from MFG programming assignment 1
+  timespec ts_start, ts_end;
+  // get start time
+  clock_gettime(CLOCK_REALTIME, &ts_start);
+
   MPI_Init(&argc, &argv);
   init_mpi();
   
@@ -161,9 +179,14 @@ int main(int argc, char *argv[]) {
   init_k = cimg_option("-n", 19,"Number of initial targets");
   const int max_iterations = cimg_option("-i", 100,"Number of iterations per broadcast loop");
   const int max_gibbs_iterations = cimg_option("-g", 100,"Number of iterations per gibbs sampling loop");
+  const double epsilon = cimg_option("-e", 1.0e-20,"stopping point for likelihood");
+  show_disp = cimg_option("-d", true,"show display");
+  const int r = cimg_option("-r", 128,"image height");
+  const int c = cimg_option("-c", 128,"image width");
+
   int image_size, target_size;
  
-  Img image(128,128,1,1), imtemp, target(19,19,1,1);
+  Img image(c,r,1,1), imtemp, target(19,19,1,1);
   if (world_rank == 0) { 
     Img image_load(filename), imtemp;
     Img target_load("images/target.bmp");
@@ -190,8 +213,7 @@ int main(int argc, char *argv[]) {
   width = image.width();
   printf("%s %d: %d x %d\n", processor_name, world_rank, width, height);
 
-  if (world_rank == 0)
-    main_disp = CImgDisplay(width,height,"Main",0);
+  if (world_rank == 0 && show_disp) main_disp = CImgDisplay(width,height,"Main",0);
   black = Img(width,height,1,1,0);
 
   if (world_rank == 0)
@@ -206,11 +228,10 @@ int main(int argc, char *argv[]) {
   gen.seed(time(0)+world_rank*100); // avoid overlapping times across machines
   boost::uniform_int<> jump(-1,1);
 
-  if (world_rank == 0)
-    main_disp.set_normalization(1);
+  if (world_rank == 0 && show_disp) main_disp.set_normalization(1);
 
   imtemp = image;
-  vector<Point> Oxy (init_k);
+  vector<Point> Oxy (0);
 
   for (int i=0; i < num_objs; i++) {
     Point point = gen_random_point(height,width);
@@ -220,8 +241,7 @@ int main(int argc, char *argv[]) {
   }
 
   // show figure
-  if (world_rank == 0)
-    imtemp.display(main_disp);
+  if (world_rank == 0 && show_disp) imtemp.display(main_disp);
   obj_fn = likelihood(image, target, Oxy, num_objs);// * pdf(pd,num_objs);
 
 //  if (world_rank == 0)
@@ -230,7 +250,7 @@ int main(int argc, char *argv[]) {
   vector<Point> Best_Oxy, CurBest_Oxy;
   double best_obj = -1.0;
 for (int i=0; i<max_iterations; i++) {
-  Oxy = gibbs_sampling(image, target, num_objs, Oxy, max_gibbs_iterations);
+  Oxy = gibbs_sampling(image, target, num_objs, Oxy, max_gibbs_iterations, (world_rank+3)*5 % 60 + 5);
   //obj_fn = likelihood(image, target, Oxy, num_objs) * pdf(pd,num_objs);
   obj_fn = likelihood(image, target, Oxy, num_objs);
   
@@ -239,20 +259,20 @@ for (int i=0; i<max_iterations; i++) {
   // everyone figures out which process has the best obj function value
   MPI_Allreduce( &maxloc, &r_maxloc, 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD );
 
+  CurBest_Oxy = Oxy;
   // the process with the best obj function value is the 4th arg here, it will be
   // the one that broadcasts its points list to the others
-  CurBest_Oxy = Oxy;
   MPI_Bcast( &CurBest_Oxy.front(), num_objs*2, MPI_INT, r_maxloc.rank, MPI_COMM_WORLD );
-  if (obj_fn/r_maxloc.val > random_uniform()) {
+  //if (obj_fn/r_maxloc.val > random_uniform()) {
     Oxy = CurBest_Oxy;
-  }
+  //}
 
   if (world_rank == 0) {
     printf("Best: %d %.5e\n", r_maxloc.rank, r_maxloc.val);
     if (r_maxloc.val > best_obj) {
       printf("^^ New winner ^^\n", r_maxloc.rank, r_maxloc.val);
       best_obj = r_maxloc.val;
-      Best_Oxy = Oxy;
+      Best_Oxy = CurBest_Oxy;
       // show image
       imtemp = image;
       for (int n=0; n < num_objs; n++) {
@@ -263,16 +283,30 @@ for (int i=0; i<max_iterations; i++) {
     for (int n=0; n < num_objs; n++) {
       imtemp2.draw_circle(CurBest_Oxy[n].x, CurBest_Oxy[n].y, 9, white, 0.9f, 1);
     }
-    main_disp.resize(256,128);
-    (imtemp,imtemp2).display(main_disp);
+    if (show_disp) {
+      main_disp.resize(512,256);
+      (imtemp,imtemp2).display(main_disp);
+    }
+  }
+  if (r_maxloc.val > epsilon) {
+    if (world_rank == 0)
+      printf("%.5e > %.5e epsilon ... breaking\n", best_obj, epsilon);
+    break;
   }
 }
 
   if (world_rank == 0) {
-    printf("Showing best\n");
-    main_disp.resize(512,512);
-    imtemp.display(main_disp,0);
+    if (best_obj > epsilon) {
+      printf("SUCCESS ");
+    } else {
+      printf("FAILURE "); // we hit max iterations
+    }
+    // get finish time and calculate running time
+    clock_gettime(CLOCK_REALTIME, &ts_end);
+    timespec ts_diff = diff(ts_start, ts_end);
+    printf("Elapsed time: %d.%09d seconds %.5e\n", ts_diff.tv_sec, ts_diff.tv_nsec, best_obj);
   }
+
   // reorder_samples
   // compute mean of samples (and round), final result
   // print final result
@@ -282,16 +316,24 @@ for (int i=0; i<max_iterations; i++) {
   // render result w/best objective function
 
   double mpi_obj_fn[36]; // XXX: hardcoded to our world max
-  MPI_Gather( &obj_fn, 1, MPI_DOUBLE, mpi_obj_fn, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD );
+  //MPI_Gather( &obj_fn, 1, MPI_DOUBLE, mpi_obj_fn, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD );
 
-  MPI_Finalize();
 
   if (world_rank == 0) {
     printf("Best: %.5e\n", best_obj);
     for (int i=0; i<num_objs; i++)
       printf("%d %d\n", Best_Oxy[i].x, Best_Oxy[i].y);
+
+    printf("Showing best\n");
+    if (show_disp) {
+      main_disp.resize(512,512);
+      imtemp.display(main_disp,0);
+    }
+
     printf("\nProgram Exit\n");
   }
+  printf("World; %d\n", world_rank);
 
+  MPI_Finalize();
   return 0;
 }
